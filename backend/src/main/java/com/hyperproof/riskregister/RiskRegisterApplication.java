92929458e8b1592ba1410c684f869ca509a2a473
shakib2501 <shakib2501@gmail.com>
2026-09-10 20:11:44 +0530

    chore: configure Spring Data JPA with H2

---
+++ backend/src/main/java/com/hyperproof/riskregister/RiskRegisterApplication.java
@@ -0,0 +1,12 @@
+package com.hyperproof.riskregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskRegisterApplication.class, args);
+    }
+}
